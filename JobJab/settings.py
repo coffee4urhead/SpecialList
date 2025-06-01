353ffd32c5587ecb70cef2cdec529bda8905935b
Mihail <dark1982soul@gmail.com>
2025-06-01 15:46:07 +0300

    adding the style reference for the website

--- JobJab/settings.py
+++ JobJab/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'JobJab.core.apps.CoreConfig',
+    "JobJab.services",
+    "JobJab.booking",
+    "JobJab.reviews",
+    "JobJab.subscriptions",
 ]
 
 MIDDLEWARE = [

1cf9f8b9d38afbdb111520b59e886ab2e8f0ee36
Mihail <dark1982soul@gmail.com>
2025-06-30 19:31:33 +0300

    adding some logic to the certificates tab on the accounts

---
+++ static/js-scripts/profile-scripts/certificate-change-script.js
@@ -0,0 +1,36 @@
+const uploaded_certificates = document.querySelectorAll('div.certificate-item');
+
+uploaded_certificates.forEach((certificate) => {
+    certificate.addEventListener('click', (e) => {
+        const previewUrl = certificate.dataset.previewUrl;
+        const title = certificate.dataset.certificateTitle;
+        const currentCertificatePreview = document.querySelector('img#certificate-file-preview');
+        const previewContainer = document.querySelector('.pdf-preview-container');
+
+        if (previewUrl) {
+            if (currentCertificatePreview) {
+                currentCertificatePreview.src = previewUrl;
+                currentCertificatePreview.alt = title;
+
+                const titleElement = currentCertificatePreview.nextElementSibling;
+                if (titleElement && titleElement.tagName === 'H2') {
+                    titleElement.querySelector('a').textContent = title;
+                }
+            } else {
+                const img = document.createElement('img');
+                img.src = previewUrl;
+                img.alt = title;
+                img.id = 'certificate-file-preview';
+
+                const titleElement = document.createElement('h2');
+                titleElement.innerHTML = `<a>${title}</a>`;
+
+                previewContainer.innerHTML = '';
+                previewContainer.appendChild(img);
+                previewContainer.appendChild(titleElement);
+            }
+        } else {
+            previewContainer.innerHTML = '<p>Preview not available</p>';
+        }
+    });
+});
